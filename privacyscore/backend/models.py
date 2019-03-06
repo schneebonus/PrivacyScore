b1aae1799a8a0376d8e6a9436d057033fac9a8ef
Mark Schneemann <git@f5w.de>
2019-03-06 01:39:26 +0100

    ...

--- privacyscore/backend/models.py
+++ privacyscore/backend/models.py
@@ -533,8 +533,6 @@ class ScanResult(models.Model):
     scan = models.OneToOneField(
         Scan, on_delete=models.CASCADE, related_name='result')
 
-    issue_checked = models.BooleanField(default=False, blank=True, null=True)
-
     result = postgres_fields.JSONField(null=True, blank=True)
 
     def __str__(self) -> str:
