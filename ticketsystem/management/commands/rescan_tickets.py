da6a6458ef11952139d1c1ec3f3883723ad39674
Mark Schneemann <git@f5w.de>
2019-03-06 13:14:24 +0100

    fix in ticket rescan

--- ticketsystem/management/commands/rescan_tickets.py
+++ ticketsystem/management/commands/rescan_tickets.py
@@ -21,8 +21,8 @@ class Command(BaseCommand):
 
             cleaned_url = self.clean_url(url)
 
-            sites_http = Site.objects.filter(url="http://" + clean_url(clean_url))
-            sites_https = Site.objects.filter(url="https://" + clean_url(clean_url))
+            sites_http = Site.objects.filter(url="http://" + cleaned_url)
+            sites_https = Site.objects.filter(url="https://" + cleaned_url)
             if len(sites_http) is not 0:
                 sites_http[0].scan()
             elif len(sites_https) is not 0:
